9ae7a5148d8b2dd31a24dcd29c31bef623054fc3
Sarvesh Kale <sarvesh.v.kale@kgpian.iitkgp.ac.in>
2023-11-14 10:56:14 +0530

    First Commit

---
+++ src/products.js
@@ -0,0 +1,45 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.webp";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.png";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Black Denim",
+    price: 599.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Women's Sparkle Shirt",
+    price: 299.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "White Hoodie",
+    price: 399.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Apple IPad",
+    price: 699.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Torn Jeans",
+    price: 199,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Beats by Dre Headphones",
+    price: 199.0,
+    productImage: product6,
+  },
+];
